8973c514d8ec407445e2a7c6ffd569d978ff23de
Christian Kwon <christiankwon@outlook.com>
2016-09-12 09:07:48 -0700

    Add random numbers to test Bubble Sort

--- Python/02 - Python Fundamentals/12 - Bubble Sort.py
+++ Python/02 - Python Fundamentals/12 - Bubble Sort.py
@@ -1,12 +1,12 @@
-def bubbleSort(ls):
-    for i in range(len(ls) - 1, 0, -1):
-        for j in range(i):
-            if ls[j] > ls[j+1]:
-                ls[j], ls[j+1] = ls[j+1], ls[j]
-    return ls
+import random
+arr = random.sample(xrange(1, 101), 100)
 
-print bubbleSort([1, 3])
-print bubbleSort([3, 2, 1])
-print bubbleSort([1,2,3,4,5,6,7,8,9])
-print bubbleSort([9,8,7,6,5,4,3,2,1])
-print bubbleSort([1,9,2,8,3,7,4,6,5])
+def bubble(arr):
+    for i in range(len(arr) - 1, 0, -1):
+        for j in range(0, i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+print arr
+print bubble(arr)
