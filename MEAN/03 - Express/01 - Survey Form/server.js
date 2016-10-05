cb60e4faaafe9c3e8aa67d4206302374fe4c494c
Christian Kwon <christiankwon@outlook.com>
2016-10-05 10:39:18 -0700

    Complete Survey Form

--- MEAN/03 - Express/01 - Survey Form/server.js
+++ MEAN/03 - Express/01 - Survey Form/server.js
@@ -1,23 +1,30 @@
-var path         = require("path");
-var express      = require('express');
-var bodyParser   = require('body-parser');
+var path       = require("path");
+var express    = require('express');
+var bodyParser = require('body-parser');
+var session    = require('express-session');
 
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, "./static")));
-
-app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+app.use(express.static(path.join(__dirname, "./static")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({secret: 'secretkey'}));
 
-app.get('/', function(req, res) {
-    res.render("index");
+app.get('/', function(request, response) {
+    response.render("index");
 });
 
-app.post('/users', function(req, res) {
-    console.log("POST DATA", req.body);
-    res.redirect('/');
+app.get('/results', function(request, response) {
+    response.render("results", request.session.post);
+});
+
+app.post('/result', function(request, response) {
+    var post = request.body;
+
+    request.session.post = post;
+
+    response.redirect('/results');
 });
 
 app.listen(8000, function() {
