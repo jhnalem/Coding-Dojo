4186c11331682bdc131b1de0b068b7fdaba96918
Christian Kwon <christiankwon@outlook.com>
2016-11-01 08:30:58 -0700

    Update C# project locations and add final two fundamental assignments

---
+++ C#/C# Fundamentals/08 - Wizard Ninja Samurai/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApplication
+{
+    public class Human
+    {
+        public string Name;
+
+        //The { get; set; } format creates accessor methods for the field specified
+        //This is done to allow flexibility
+        public int Health { get; set; }
+
+        //These properties are all private
+        protected int Strength { get; set; }
+        protected int Intelligence { get; set; }
+        protected int Dexterity { get; set; }
+
+        public Human(string name)
+        {
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            Health = 100;
+        }
+
+        public Human(string name, int str, int intel, int dex, int hp)
+        {
+            Name = name;
+            Strength = str;
+            Intelligence = intel;
+            Dexterity = dex;
+            Health = hp;
+        }
+
+        public void Attack(object obj)
+        {
+            Human enemy = obj as Human;
+            if(enemy == null)
+            {
+                Console.WriteLine("Failed Attack");
+            }
+            else
+            {
+                enemy.Health -= Strength * 5;
+            }
+        }
+    }
+
+    public class Wizard : Human
+    {
+        public Wizard(string name) : base(name, 3, 25, 3, 50)
+        {
+
+        }
+
+        public Wizard(string name, int str, int intel, int dex, int hp) : base(name, str, intel, dex, hp)
+        {
+            
+        }
+
+        public void Heal()
+        {
+            Health += Intelligence * 10; 
+        }
+
+        public void Fireball(Object obj)
+        {
+            Human enemy = obj as Human;
+            if(enemy == null)
+            {
+                Console.WriteLine("Fireball failed!");
+            }
+            else
+            {
+                Random rng = new Random();
+                enemy.Health -= rng.Next(20, 51);
+            }
+        }
+    }
+
+    public class Ninja : Human
+    {
+        public Ninja(string name) : base(name, 3, 3, 175, 100)
+        {
+
+        }
+
+        public Ninja(string name, int str, int intel, int dex, int hp) : base(name, str, intel, dex, hp)
+        {
+            
+        }
+
+        public void Steal(Object obj)
+        {
+            Human enemy = obj as Human;
+            if(enemy == null)
+            {
+                Console.WriteLine("Steal failed!");
+            }
+            else
+            {
+                enemy.Health -= 10;
+                Health += 10;
+            }
+        }
+
+        public void GetAway()
+        {
+            Health -= 15;
+        }
+    }
+
+    public class Samuri : Human
+    {
+        static int Count;
+        public Samuri(string name) : base(name, 3, 3, 3, 200)
+        {
+            Interlocked.Increment(ref Count);
+        }
+
+        public Samuri(string name, int str, int intel, int dex, int hp) : base(name, str, intel, dex, hp)
+        {
+            Interlocked.Increment(ref Count);
+        }
+
+        public void DeathBlow(Object obj)
+        {
+            Human enemy = obj as Human;
+            if (enemy == null)
+            {
+                Console.WriteLine("Death blow failed!");
+            }
+            else
+            {
+                if (enemy.Health < 50)
+                {
+                    enemy.Health = 0;
+                }
+            }
+        }
+
+        public void Meditate()
+        {
+            Health = 200;
+        }
+
+        public void HowMany()
+        {
+            Console.WriteLine("There are {0} samuri.", Count);
+        }
+    }
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var A = new Samuri("A");
+            var B = new Samuri("B");
+            var C = new Samuri("C");
+            var D = new Samuri("D");
+            D.HowMany();
+        }
+    }
+}
---
+++ C#/C# Fundamentals/09 - ConsoleDB/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleWithDb;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        private static ConsoleWithDb.DbConnector Q = new ConsoleWithDb.DbConnector();
+
+        public static List<Dictionary<string, object>> displayUsers() {
+            var result = Q.ExecuteQuery("SELECT * FROM users");
+
+            foreach( var user in result ) {
+                Console.WriteLine("ID: {0}; Name: {1} {2}; Favorite Number: {3}", user["id"], user["firstName"], user["lastName"], user["favoriteNumber"]);
+            }
+
+            return result;
+        }
+
+        public static List<Dictionary<string, object>> insert(string fname, string lname, int favNum) {
+            string query = String.Format(@"INSERT INTO users (firstName, lastName, favoriteNumber)
+                                           VALUES ('{0}', '{1}', '{2}')", fname, lname, favNum);
+
+            var result = Q.ExecuteQuery(query);
+
+            displayUsers();
+
+            return result;
+        }
+
+        public static List<Dictionary<string, object>> update(int id, string fname, string lname, int favNum) {
+            string query = String.Format(@"UPDATE users
+                                           SET firstName='{0}', lastName='{1}', favoriteNumber='{2}'
+                                           WHERE id='{3}'", fname, lname, favNum, id);
+
+            var result = Q.ExecuteQuery(query);
+
+            displayUsers();
+
+            return result;
+        }
+
+        public static void delete(int id) {
+            string query = String.Format(@"DELETE FROM users
+                                           WHERE id='{0}'", id);
+            Q.ExecuteQuery(query);
+
+            displayUsers();
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("All Users *********************************");
+            displayUsers();
+
+            // Console.WriteLine("After Insert ******************************");
+            // insert("First", "Last", 4);
+
+            // Console.WriteLine("After Update ******************************");
+            // update(4, "C", "D", 5);
+
+            // Console.WriteLine("After Delete ******************************");
+            // delete(4);
+        }
+    }
+}
